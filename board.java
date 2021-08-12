7c21991052bab876a2cf02d47c01990d42c3afd4
Oliverkor10 <okorchno@ucsd.edu>
2021-08-11 20:56:09 -0700

    Made my own custom Board Object

--- board.java
+++ board.java
@@ -1,100 +1,90 @@
-import java.io.*;
+/**
+ * Oliver's version of a board object
+ */
+public class Board {
+    public static final int X = 1;
+    public static final int O = 2;
 
-class Board
-{
-    private final static int _ = 0;
-    private final static int O = 1;
-    private final static int X = 2;
-    
-    private int[][] board;
-    private int bsize;
-    private boolean gameover;
-
-    public Board() {
-        // initialize board
-        for (int i = 0; i < bsize; i++) {
-            for (int j = 0; j < bsize; i++) {
-                board[i][j] = _;
-            }
-        }
-        this.bsize = 3;
-        this.gameover = false;
-    }
+    //instance variables
+    int[] board;
+    boolean gameOver;
+    int colSize;
+    int rowSize;
+    char winner;
 
     /**
-     * Make a copy of the current board
-     * @return
+     * Default Board Constructer
      */
-    public Board copy(){
-        Board newBoard = new Board();
-        for(int i=0; i<this.board.length; i++){
-            for(int j = 0; j < this.board[0].length; j++){
-                newBoard.board[i][j] = this.board[i][j];
+    public Board(){
+        this.colSize = 3;
+        this.rowSize = 3;
+        for(int i = 0; i < colSize*rowSize; i++){
+            board[i] = 0;
+        }
+        this.gameOver = false;
+        this.winner = 0;
+    }
+
+    public int index(int x, int y){
+        return (this.colSize * y + x);
+    }
+
+    public void checkGameOver(){
+        //Condition 1: Diagonals
+        if(board[0]!=0 && board[0] == board[4] && board[4] == board[8]){
+            this.gameOver = true;
+            if(board[0] == 1){
+                this.winner = 'X';
+            }
+            else{
+                this.winner = 'O';
             }
         }
-        newBoard.bsize = this.bsize;
-        newBoard.gameOver = this.gameOver;
-    }
-
-    /** Inputs the turn onto the board. Returns 1 is successful
-      * char - the player whose turn it is
-      * x - the column on the board
-      * y - the row on the board
-    */
-    public boolean turn(int char, int x, int y) {
-        if (board[x][y] == _) {
-            board[x][y] = char;
-            return true;
-        }
-        return false;
-    }
-
-    public boolean isGameOver() {
-        int x = y = 0;
-        int first,second,third = 0;
-
-        // vertical wins
-        for (x = 0; x < 3; x++) {
-            first = board[x][0];
-            second = board[x][1];
-            third = board[x][2];
-
-            if (first == second && second == third) {
-                gameover = true;
-                return true;
+        if(board[2]!=0 && board[2] == board[4] && board[4] == board[6]){
+            this.gameOver = true;
+            if(board[0] == 1){
+                this.winner = 'X';
+            }
+            else{
+                this.winner = 'O';
             }
         }
 
-        // horizontal wins
-        for (y = 0; y < 3; y++) {
-            first = board[y][0];
-            second = board[y][1];
-            third = board[y][2];
-
-            if (first == second && second == third) {
-                gameover = true;
-                return true;
+        int first;
+        int second;
+        int third;
+        //vertical win
+        for(int x=0; x<3; x++){
+            first = board[index(x,0)];
+            second = board[index(x,1)];
+            third = board[index(x,2)];
+            if(first != 0 && first == second && second == third){
+                if(first == 1){
+                    this.winner = 'X';
+                }
+                else{
+                    this.winner = 'O';
+                }
+                this.gameOver = true;
             }
         }
 
-        // diagonal wins
-        first = board[0][0];
-        second = board[1][1];
-        third = board[2][2];
-        if (first == second && second == third) {
-            gameover = true;
-            return true;
+        //horizontal win
+        for(int y=0; y<3; y++){
+            first = board[index(0,y)];
+            second = board[index(1,y)];
+            third = board[index(2,y)];
+            if(first != 0 && first == second && second == third){
+                if(first == 1){
+                    this.winner = 'X';
+                }
+                else{
+                    this.winner = 'O';
+                }
+                this.gameOver = true;
+            }
         }
 
-        first = board[2][0];
-        second = board[1][1];
-        third = board[0][2];
-        if (first == second && second == third) {
-            gameover = true;
-            return true;
-        }
-        return false;
+
     }
-
-
 }
